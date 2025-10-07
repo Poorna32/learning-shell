287d5a3fbe345c604ab1e881412c423f30a73fcd
Poorna32 <poornachandraga44@gmail.com>
2025-10-07 20:49:06 +0200

    Learning shell ifelse  condition

--- ifesle.Conditions.sh
+++ ifesle.Conditions.sh
@@ -9,14 +9,7 @@ EOF
 x=$1
 if [ "$x" -gt 1000 ]; then
   echo "$x is Greater than 1000"
+  exit 1
 else
   echo "$x is Lesser than 1000"
-fi
-
-
-x=$1
-if [ "$x" -eq 500 ]; then
-  echo "$x is Equal to 500"
-else
-  echo "$x is NotEqual to 500"
 fi
