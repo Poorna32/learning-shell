eb986b9a8e13caa9902c014ad88e065ef0673cfa
Poorna32 <poornachandraga44@gmail.com>
2025-10-07 21:19:07 +0200

    Learning shell ifelse  condition

--- ifesle.Conditions.sh
+++ ifesle.Conditions.sh
@@ -6,12 +6,9 @@ else
 fi
 EOF
 
-
-#Handling the Input Missing suing exit status command
 x=$1
 if [ "$x" -gt 1000 ]; then
   echo "$x is Greater than 1000"
-  exit 1
 else
   echo "$x is Lesser than 1000"
 fi
